9aa67c16e2dad9269b6852f4acfd709b144e70f2
Aishika Midder <aishikamidder50@gmail.com>
2020-09-10 23:31:11 +0530

    first commit

---
+++ pomodoroclock.py
@@ -0,0 +1,21 @@
+import os, time, requests
+from boltiot import Sms
+
+# later use time to avoid alerts at times you are busy
+
+def send_simple_message():
+	return requests.post(
+		"https://api.mailgun.net/v3/{}/messages".format(os.environ['MAILGUN_URL']),
+		auth=("api", os.environ['MAILGUN_API_KEY']),
+		data={"from": "User Name <mailgun@{}>".format(os.environ['MAILGUN_URL']),
+			"to": [os.environ['RECIPIENT_EMAIL']],
+			"subject": "ALERT",
+			"text": "Testing Mailgun!"})
+
+if(os.environ['IS_ACTIVATED']=='1'):
+    response=send_simple_message()
+    print("Response EMAIL",response)
+
+    sms = Sms(os.environ['SSID'], os.environ['AUTH_TOKEN'], os.environ['TO_NUMBER'], os.environ['FROM_NUMBER'])
+    response = sms.send_sms("Testing")
+    print("Response SMS",response)
